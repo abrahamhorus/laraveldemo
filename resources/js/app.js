abdc736527b2a638e41223fe43fd9dd39386a45a
abrahamhorus <110262126+abrahamhorus@users.noreply.github.com>
2023-02-26 18:22:45 -0600

    se completo el paso 2

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import App from '../src/App.vue';
+import '../css/app.css';
+createApp(App).mount('#app');
---
+++ resources/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+  <h1>BIENVENIDOS AL EJERCICIO DE LARAVEL</h1>
+  <hr>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h1{
+  text-align: center;
+}
+div{
+  background-color: rgba(0, 255, 136, 0.308);
+}
+</style>
